67086b2b3f51a73b882c46ee3687da314c853029
littleRabbit0220 <bluecoder0220@gmail.com>
2023-06-21 07:59:45 +0300

    project created.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const axios  = require('axios');
+
+app.get('/getData', (req, res) => {
+  axios.get("https://raritysniper.com/nft-drops-calendar")
+  .then(response => {
+    console.log(response)
+    res.send(response.data);
+  })
+  .catch(err => res.send(err))
+  
+});
+
+app.listen(5000,() => {
+  console.log('server is running.');
+});
